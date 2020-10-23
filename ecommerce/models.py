1d8d4f4293b5f2970c00ea410e8e475264ec2600
guicavalheiro <cavalheiro.dev@gmail.com>
2020-10-23 01:46:43 -0300

    finished reports to txt

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,8 +1,10 @@
 import json
+#import pandas as pd
+from django.shortcuts import render
 
 def mount_data():
     
-    with open('ecommerce/data/data.json') as json_file:
+    with open('D:/Desktop/Pucrs/2020_2/ProjArq/MVC-django/e-commerce/ecommerce/data/data.json') as json_file:
         data = json.load(json_file)
     
     return data
@@ -10,5 +12,54 @@ def mount_data():
 def add_pedido(store):
 
     data = mount_data()
-    
     print(data[store])
+
+def store_report(request):
+    data = mount_data()
+    
+    store_list = []
+    for store in data:
+        for info_loja in data[store]:        
+            if info_loja == 'pedidos':
+                for pedido in data[store][info_loja]:
+                    nome_loja = data[store]['loja']
+                    #print(f'Loja: {nome_loja} -- Produto: {data[store][info_loja][pedido][0]} Preço: {data[store][info_loja][pedido][1]} Data de Entrega: {data[store][info_loja][pedido][2]}')
+                    store_list.append((nome_loja, data[store][info_loja][pedido][0], float(data[store][info_loja][pedido][1]), data[store][info_loja][pedido][2]))
+    
+    with open('D:/Desktop/Pucrs/2020_2/ProjArq/MVC-django/e-commerce/ecommerce/reports/sorted_by_store.txt', 'w') as sorted_w:
+        for item in store_list:
+            sorted_w.write("Loja: " + item[0] + " Produto: " + item[1] + " Preco: " + str(item[2]) + " Data: " + item[3] + "\n")
+
+    return render(None, 'D:/Desktop/Pucrs/2020_2/ProjArq/MVC-django/e-commerce/ecommerce/templates/report.html')
+
+def date_report(request):
+    #print("date")
+    data = mount_data()
+    
+    return render(None, 'D:/Desktop/Pucrs/2020_2/ProjArq/MVC-django/e-commerce/ecommerce/templates/report.html')
+
+def price_report(request):
+    #print("price")
+    data = mount_data()
+    
+    price_list = []
+    for store in data:
+        for info_loja in data[store]:        
+            if info_loja == 'pedidos':
+                for pedido in data[store][info_loja]:
+                    nome_loja = data[store]['loja']
+                    #print(f'Loja: {nome_loja} -- Produto: {data[store][info_loja][pedido][0]} Preço: {data[store][info_loja][pedido][1]} Data de Entrega: {data[store][info_loja][pedido][2]}')
+                    price_list.append((nome_loja, data[store][info_loja][pedido][0], float(data[store][info_loja][pedido][1]), data[store][info_loja][pedido][2]))
+    
+    price_list.sort(key=lambda tup: tup[2])
+    
+    with open('D:/Desktop/Pucrs/2020_2/ProjArq/MVC-django/e-commerce/ecommerce/reports/sorted_by_price.txt', 'w') as sorted_w:
+        for item in price_list:
+            sorted_w.write("Loja: " + item[0] + " Produto: " + item[1] + " Preco: " + str(item[2]) + " Data: " + item[3] + "\n")
+    
+    return render(None, 'D:/Desktop/Pucrs/2020_2/ProjArq/MVC-django/e-commerce/ecommerce/templates/report.html')    
+if __name__ == "__main__":
+    
+    store_report()
+    #date_report()
+    price_report()
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from . import models
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -9,5 +10,8 @@ urlpatterns = [
     url(r'^order/', TemplateView.as_view(template_name='order.html')),
     url(r'^register/', views.request_page),
     url(r'^report/', TemplateView.as_view(template_name='report.html')),
+    url(r'^store/', models.store_report),
+    url(r'^date/', models.date_report),
+    url(r'^price/', models.price_report),
     url(r'^test/', views.test),
 ]
